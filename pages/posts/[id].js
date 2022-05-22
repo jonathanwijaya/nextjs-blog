58b5502e3ec48264d7450ad630241abd79869ac5
jonathanwijaya <jonathanwijaya961@gmail.com>
2022-05-23 00:43:14 +0700

    add tailwind configuration

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -27,7 +27,7 @@ export default function Post({ postData }) {
                 <title>{postData.title}</title>
             </Head>
             <article>
-                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <h1 className={'text-3xl font-bold underline'}>{postData.title}</h1>
                 <div className={utilStyles.lightText}>
                     <Date dateString={postData.date} />
                 </div>
